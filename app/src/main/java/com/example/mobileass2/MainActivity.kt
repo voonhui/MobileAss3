6199b0a15a4defdb04fd42fca98a2dfca20bd0ab
voonhui <leongvh-wm19@student.tarc.edu.my>
2022-04-15 00:48:14 +0800

    test4

--- app/src/main/java/com/example/mobileass2/MainActivity.kt
+++ app/src/main/java/com/example/mobileass2/MainActivity.kt
@@ -20,7 +20,6 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
-import android.content.ContentValues.TAG
 import android.util.Log
 import android.content.ContentValues
 
