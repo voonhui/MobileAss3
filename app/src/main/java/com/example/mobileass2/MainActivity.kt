b1bfdd8fc0e9fbf3b81cbe48d08b018ee845504b
voonhui <leongvh-wm19@student.tarc.edu.my>
2022-04-14 18:08:03 +0800

    initial bottom navigation and no action bar soli

---
+++ app/src/main/java/com/example/mobileass2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.mobileass2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setupWithNavController(bottomNavigationView, navController)
+    }
+
+
+    fun handleSelection(view: View) {
+        Toast.makeText(this, "Image is tapped", Toast.LENGTH_LONG).show()
+    }
+}
