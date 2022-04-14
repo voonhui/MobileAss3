00b1f1b48614941255e5aea46a5efb6f85e28708
voonhui <leongvh-wm19@student.tarc.edu.my>
2022-04-14 19:32:43 +0800

    test 2

--- app/src/main/java/com/example/mobileass2/MainActivity.kt
+++ app/src/main/java/com/example/mobileass2/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -18,12 +23,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
         navController = navHostFragment.navController
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         setupWithNavController(bottomNavigationView, navController)
+
+        val navController = findNavController(R.id.container)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.sport_nav, R.id.diet_nav, R.id.profile_nav
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupWithNavController(bottomNavigationView, navController)
     }
+
+
 
 
     fun handleSelection(view: View) {
